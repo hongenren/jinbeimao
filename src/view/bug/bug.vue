c0266a8629761507940942b401f26d742e203109
用户hongenren <274095759@qq.com>
2019-08-15 10:04:02 +0800

    bug

---
+++ src/view/bug/bug.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+<i class="fa fa-bug" aria-hidden="true"></i>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
